40ff1a8758742bfa4cb20c454053253798522112
MamadAli <mhmd.ali.shabani@gmail.com>
2021-01-12 15:22:33 +0330

    create multiple model

--- base/Model.php
+++ base/Model.php
@@ -1,12 +1,40 @@
 <?php
 namespace mamadali\base;
 
-use yii\base\Component;
+use yii\helpers\ArrayHelper;
 
 /**
  *
  */
-class Model extends Component
+class Model extends \yii\base\Model
 {
+
+    /**
+     * Creates and populates a set of models.
+     *
+     * @param string $modelClass
+     * @param array $multipleModels
+     * @return array
+     */
+    public static function createMultiple($modelClass, $multipleModels = [])
+    {
+        $model    = new $modelClass;
+        $formName = $model->formName();
+        $models   = [];
+
+        if ($multipleModels && is_array($multipleModels)) {
+            foreach ($multipleModels as $i => $item) {
+                if (isset($item['id']) && !empty($item['id']) && isset($multipleModels[$item['id']])) {
+                    $models[] = $multipleModels[$item['id']];
+                } else {
+                    $models[] = new $modelClass;
+                }
+            }
+        }
+
+        unset($model, $formName, $post);
+
+        return $models;
+    }
 
 }
